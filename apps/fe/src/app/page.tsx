c1c278be28e265e4f89c09133b3a4411575e3533
Reza SOLTAN <soltan-reza@outlook.com>
2026-02-11 12:56:32 +0100

    fix: Fixed fe project initialization and added docker compose file and envs.

--- apps/fe/src/app/page.tsx
+++ apps/fe/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome my-new-app 👋
+              Welcome fe 👋
             </h1>
           </div>
 
@@ -397,7 +397,7 @@ export default function Index() {
                 </svg>
                 View project details
               </summary>
-              <pre>nx show project my-new-app --web</pre>
+              <pre>nx show project fe --web</pre>
             </details>
             <details>
               <summary>
